91f9dbf3b5691dc8057c6572be5dbc7ae139839c
jmatics <jgeomatics@gmail.com>
2019-02-18 17:06:13 +0100

    "DOY" (julian date) was introduced.

--- pls_with_all_100times.R
+++ pls_with_all_100times.R
@@ -40,111 +40,9 @@ dsb_df$source <- rep("Rhön", dim(dsb_df)[1])
 all_df <- rbind(wiz_df, dsb_df)
 
 # Define seed values
-# 100 numbers generated between 1 and 100000
+# 100 numbers generated between 1 and 99999
 
-seedVec <-
-  c(
-    47999,
-    86664,
-    9896,
-    81577,
-    87574,
-    60874,
-    8445,
-    61601,
-    12542,
-    84331,
-    92156,
-    42532,
-    54205,
-    77947,
-    35020,
-    47384,
-    96268,
-    99501,
-    13234,
-    30640,
-    47115,
-    51293,
-    25621,
-    69429,
-    89133,
-    50920,
-    36120,
-    52283,
-    50955,
-    28713,
-    13129,
-    73411,
-    55214,
-    3118,
-    15432,
-    96425,
-    93839,
-    67354,
-    27001,
-    46678,
-    7002,
-    78738,
-    9028,
-    24213,
-    54594,
-    67920,
-    67009,
-    18928,
-    59971,
-    85079,
-    27368,
-    70443,
-    24754,
-    91252,
-    26147,
-    22559,
-    86933,
-    7910,
-    95300,
-    19866,
-    48144,
-    52590,
-    35932,
-    67085,
-    10149,
-    18486,
-    95006,
-    445,
-    83360,
-    66332,
-    38214,
-    74489,
-    53402,
-    1486,
-    5843,
-    83213,
-    17498,
-    13772,
-    3995,
-    20366,
-    36259,
-    87131,
-    30907,
-    19117,
-    77909,
-    11248,
-    24335,
-    99879,
-    21860,
-    82767,
-    51555,
-    90820,
-    16708,
-    35086,
-    32838,
-    30931,
-    68453,
-    57428,
-    61707,
-    89342
-  )
+seedVec <- sample(c(1:99999), 100, replace=FALSE) 
 
 # set empty list for outputs
 
@@ -157,7 +55,6 @@ op_n <- vector("list", 100)
 
 plsFit_adf <- vector("list", 100)
 op_adf <- vector("list", 100)
-
 
 
 # Model calibrtion and validation 100 times
@@ -196,9 +93,6 @@ for (k in 1:length(seedVec)) {
       all_df,
       p = 0.8,
       cat_col = "field_id",
-      num_col = NULL,
-      id_col = NULL,
-      id_aggregation_fn = sum,
       force_equal = FALSE,
       list_out = F
     )
@@ -258,18 +152,22 @@ for(i in 1:100){
   mean_n[i] <- mean(test_list[[i]][,targetN])
 }
 
-
-boxplot((unlist(lapply(op_n,"[[","RMSE"))/mean_n)*100)
 # Results for ADF
 
-boxplot(unlist(lapply(op_adf,"[[","RMSE"))) 
-boxplot(unlist(lapply(plsFit_adf,"[[","bestTune"))) 
+boxplot(unlist(lapply(op_adf, "[[", "RMSE")))
+boxplot(unlist(lapply(plsFit_adf, "[[", "bestTune")))
 
 mean_adf <- numeric()
-for(i in 1:100){
-  mean_adf[i] <- mean(test_list[[i]][,targetADF])
+for (i in 1:100) {
+  mean_adf[i] <- mean(test_list[[i]][, targetADF])
 }
 
-boxplot((unlist(lapply(op_adf,"[[","RMSE"))/mean_adf)*100)
 
+# rRMSEP plots
+par(mfrow = c(1, 2))
+boxplot((unlist(lapply(op_n, "[[", "RMSE")) / mean_n) * 100, main = "N",
+        ylab = "rRMSEP (%)")
+boxplot((unlist(lapply(op_adf, "[[", "RMSE")) / mean_adf) * 100, main = "ADF",
+        ylab = "rRMSEP (%)")
+par(mfrow = c(1, 1))
 
