a0a1d35a87b565875b58cd4c79785f147d4d88c0
jmatics <33061273+jmatics@users.noreply.github.com>
2019-02-19 09:40:35 +0100

    Add descriptive comments

--- funs/spc_2df.R
+++ funs/spc_2df.R
@@ -1,3 +1,10 @@
+###################################################################################
+# Function to convert `hsdar::speclib` object to a dataframe
+# Original snippet from https://rdrr.io/github/ssdxj/G407/src/R/ulits4spc.R
+# Edited by Jayan Wijesingha (jayan.wijesingha@uni-kassel.de)
+# 2019-02-10
+###################################################################################
+
 spc_2df <- function(spc) {
   out <- NULL
   attri <- SI(spc)
